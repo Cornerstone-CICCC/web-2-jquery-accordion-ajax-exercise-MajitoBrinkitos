fe1d32384c3a481f702e6723caf4a74e7aa0c5e7
Majito Brinkitos <mcvef2013@gmail.com>
2024-11-28 19:52:15 -0800

    2024-11-28

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-//Exercise 1
+'use strict';
+const url = 'https://dummyjson.com/todos';
 
+//Exercise 1
 $(function() {
   console.log("Exercise #1 is working!");
 
@@ -8,3 +10,26 @@ $(function() {
     $(this).next('.accordion-content').slideToggle(1000);
   });
 });
+
+//Exercise 2
+//request from https://dummyjson.com/todos
+$(function(){
+  $('button').click(function(){
+    $.ajax({
+      url: 'https://dummyjson.com/todos',
+      success: function(result) {
+        const todoList = $('.todos ul');
+        todoList.empty();
+  
+        result.todos.forEach(todo => {
+          const listItem = $('<li></li>').text(todo.todo);
+          todoList.append(listItem);
+        });
+      },
+        error: function(xhr, status, error) {
+          $('.todos ul').html(`<li>Error: ${xhr.status} ${error}</li>`)
+        }
+    });
+  });
+});
+
